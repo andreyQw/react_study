cb1dcfefbd0550f08f86fc3d6b49f762fa8c5dd7
andrey.bogdanovskiy <bogdanovskiya@gmail.com>
2020-03-10 16:52:07 +0200

    CSS added Radium component (24)

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Radium from "radium"
 import "./Car.css";
 
-function Car(props) {
+const Car = props => {
   const inputClasses = ["input"];
 
   if (props.name !== "") {
@@ -14,8 +15,17 @@ function Car(props) {
     inputClasses.push("bold");
   }
 
+  const style = {
+    border: '1px solid #ccc',
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+    ":hover": {
+      border: '1px solid $aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+      cursor: 'pointer'
+    }
+  }
   return (
-    <div className="Car">
+    <div className="Car" style={style}>
       <h3>Car name: {props.name}</h3>
       <p>
         Year: <strong>{props.year}</strong>
@@ -31,4 +41,4 @@ function Car(props) {
   );
 }
 
-export default Car;
+export default Radium(Car);
